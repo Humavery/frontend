9d7077fbb877b65fb999a6a5e53325e9ddc84e2c
Oliwier <m0lc14kk@outlook.com>
2026-04-26 14:27:37 +0200

    refactor(codebase): cleaned up `public` directory; cleaned up `src` content

---
+++ src/libs/models/components/general/interfaces/IChildren.ts
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+/**
+ * @summary Interface for components that have children.
+ * @description This interface is used to define the children of a component. Extend this interface to add children to your component.
+ */
+interface IChildren {
+    /**
+     * @summary The children of the component.
+     * @description The children of the component. This is the content that will be rendered inside the component.
+     */
+    readonly children: ReactNode
+}
+
+export type { IChildren }
