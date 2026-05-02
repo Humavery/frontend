2c75c4ee1a8c6dab822289d318144f8f6ae05b9f
Oliwier <m0lc14kk@outlook.com>
2026-05-03 00:00:06 +0200

    feat(nextjs): allowed remote pattern (CDN server) to be used within images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next"
 
 /**
+ * @summary The CDN endpoint URL.
+ * @description Parses the CDN endpoint URL from the environment variables and returns it if it's valid.
+ * @throws {Error} If the CDN endpoint URL is not set or is invalid.
+ */
+const CDN_ENDPOINT_URL: URL = ((): URL => {
+    const value: string | undefined = process.env.NEXT_PUBLIC_CDN_ENDPOINT_URL
+    if (value === undefined) throw new Error("NEXT_PUBLIC_CDN_ENDPOINT_URL is not set.")
+
+    try {
+        return new URL(value)
+    } catch {
+        throw new Error("NEXT_PUBLIC_CDN_ENDPOINT_URL is not a valid URL.")
+    }
+})()
+
+/**
  * @summary Next.js configuration.
  * @description Configuration of the frontend application.
  * @see {@link https://nextjs.org/docs/app/api-reference/config/next-config-js}
@@ -13,6 +29,14 @@ export default {
         fetches: {
             fullUrl: false,
         },
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: CDN_ENDPOINT_URL.hostname,
+            },
+        ],
     },
     async redirects() {
         return [
