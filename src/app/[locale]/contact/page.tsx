ae627e165c2166bda9decf0d409d82e4681ce890
Pitulec <96929600+Pitulec@users.noreply.github.com>
2026-06-09 11:55:16 +0200

    feat(contact): added first section badge, heading and subtext

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,6 +1,8 @@
 import type { JSX } from "react"
 import CommonNavigationBarComponent from "@/humavery/components/general/layout/common/navigation/CommonNavigationBarComponent"
-
+import Heading from "@/humavery/components/general/ui/Heading"
+import Subtext from "@/humavery/components/general/ui/Subtext"
+import Badge from "@/humavery/components/general/ui/Badge"
 /**
  * @summary The contact page.
  * @description This page is used to display the contact page.
@@ -10,7 +12,20 @@ function ContactPage(): JSX.Element {
     return (
         <>
             <CommonNavigationBarComponent />
-            <main className="w-full"></main>
+            <section className="w-full min-h-dvh bg-[url(/assets/images/background.png)] bg-cover bg-center">
+                <div className="flex flex-col items-center justify-center m-auto w-full max-w-4xl px-4 pt-18 gap-6">
+                    <Badge>Jesteśmy dostępni</Badge>
+                    <Heading>Skontaktuj się z nami</Heading>
+                    <Subtext className="max-w-xl">
+                        Jeśli masz pytanie, potrzebujesz pomocy, jesteśmy do Twojej dyspozycji. Skontaktuj się z nami za
+                        pomocą poniższego formularza kontaktowego, lub bezpośrednio przez{" "}
+                        <a href="mailto:contact@humavery.com" className="text-blue-500 hover:underline">
+                            e-mail
+                        </a>
+                        .
+                    </Subtext>
+                </div>
+            </section>
         </>
     )
 }
