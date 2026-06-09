bbeaa07a19b8bd83ad28b637ec4018cc4203ad74
Pitulec <96929600+Pitulec@users.noreply.github.com>
2026-06-09 12:09:47 +0200

    feat(Heading): changed className font-bold -> font-medium

--- src/components/general/ui/Heading.tsx
+++ src/components/general/ui/Heading.tsx
@@ -1,7 +1,7 @@
 export default function Heading({ children, className = "" }) {
     return (
         <h1
-            className={`text-5xl sm:text-7xl font-bold text-center text-neutral-950 tracking-tight leading-tight ${className}`}>
+            className={`text-5xl sm:text-7xl font-medium text-center text-neutral-950 tracking-tight leading-tight ${className}`}>
             {children}
         </h1>
     )
